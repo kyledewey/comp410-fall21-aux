d786c8271e3b9dc9bafd4a93a62ff9c336767827
Kyle Dewey <kyle.dewey@csun.edu>
2021-09-14 16:50:30 -0700

    prolog code intro

---
+++ prolog/intro.pl
@@ -0,0 +1,16 @@
+% procedure - isInteger/1
+% arity: number of parameters
+isInteger(0). % fact
+isInteger(1). % fact
+isInteger(2). % fact
+
+% closed-world assumption
+
+% procedure - isName/1
+% alice and bob are atoms
+isName(alice). % fact
+isName(bob).   % fact
+
+% procedure - someFact/1
+someFact(foo).
+
