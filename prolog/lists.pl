3fca4911d8c6d66b003c38fb498cb735d6fc4ff0
Kyle Dewey <kyle.dewey@csun.edu>
2021-10-12 16:40:58 -0700

    unoptimized append

---
+++ prolog/lists.pl
@@ -0,0 +1,20 @@
+% cons(1, cons(2, cons(3, nil)))
+% Base case: putting nil on the front of another list
+% Recursive case:
+%
+% [1, 2, 3] ++ [4, 5, 6] ==> 1 :: ([2, 3] ++ [4, 5, 6])
+%
+% myAppend: InputList1, InputList2, OutputList
+myAppend(nil, OtherList, OtherList).
+myAppend(cons(Head, Tail), OtherList, ResultList) :-
+    myAppend(Tail, OtherList, IntermediateList),
+    ResultList = cons(Head, IntermediateList).
+
+
+someFact(1).
+someFact(2).
+someFact(3).
+
+foo(X, Y) :-
+    Z = X,
+    Z = Y.
