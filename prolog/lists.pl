df21bafd16fe2e4590b571622094104b5b17f797
Kyle Dewey <kyle.dewey@csun.edu>
2021-10-14 15:51:19 -0700

    long version of append

--- prolog/lists.pl
+++ prolog/lists.pl
@@ -4,10 +4,45 @@
 %
 % [1, 2, 3] ++ [4, 5, 6] ==> 1 :: ([2, 3] ++ [4, 5, 6])
 %
+%
+% myAppend([1, 2, 3], [4, 5, 6]) ==>
+%   1 :: myAppend([2, 3], [4, 5, 6]): [2, 3, 4, 5, 6]
+%   Cons(1, myAppend([2, 3], [4, 5, 6]))
+%
+% def myAppend(list1, list2):
+%     if isinstance(list1, Nil):
+%         return list2
+%     else: # must be a cons
+%         rest = myAppend(list1.tail, list2)
+%         return Cons(list1.head, rest)
+
+myAppend(List1, List2, Result) :-
+    List1 = nil,
+    Result = List2.
+myAppend(List1, List2, Result) :-
+    List1 = cons(Head, Tail),
+    myAppend(Tail, List2, Rest),
+    Result = cons(Head, Rest).
+
+
+
+
+
+
+
+
+%
+%
+% if (base case) {
+%   // do base case stuff
+% } else {
+%   // do recursive case stuff
+% }
+%
 % myAppend: InputList1, InputList2, OutputList
-myAppend(nil, OtherList, OtherList).
-myAppend(cons(Head, Tail), OtherList, cons(Head, IntermediateList)) :-
-    myAppend(Tail, OtherList, IntermediateList).
+%% myAppend(nil, OtherList, OtherList). % base case
+%% myAppend(cons(Head, Tail), OtherList, cons(Head, IntermediateList)) :- % recursive case
+%%     myAppend(Tail, OtherList, IntermediateList).
 
 someFact(1).
 someFact(2).
