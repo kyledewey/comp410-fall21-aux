ff84392b07911648cef003767e4285fa6d332f7b
Kyle Dewey <kyle.dewey@csun.edu>
2021-10-28 16:48:27 -0700

    more comments

--- prolog/accumulators.pl
+++ prolog/accumulators.pl
@@ -1,6 +1,10 @@
+% Procedure: facts/rules with the same name and arity
+% arity: the number of parameters the fact/rule takes
+% reversed/3
 reversed([], Accumlator, Accumlator).
 reversed([Head | Tail], Accumlator, Result) :-
     reversed(Tail, [Head | Accumlator], Result).
 
+% reversed/2
 reversed(List, Result) :-
     reversed(List, [], Result).
